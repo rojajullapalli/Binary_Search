012327110f44b78444b69678569f0ed889839885
rojajullapalli <rojajules@gmail.com>
2021-08-29 08:23:13 +0530

    ADDED: Binary Search Tree

--- src/com/bridgelabz/binary/BinaryMain.java
+++ src/com/bridgelabz/binary/BinaryMain.java
@@ -3,5 +3,9 @@ package com.bridgelabz.binary;
 public class BinaryMain {
     public static void main(String[] args) {
         System.out.println("welcome to binary search program");
+        BinarySearchTree<Integer> myBinaryTree = new BinarySearchTree();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
     }
 }
---
+++ src/com/bridgelabz/binary/MyBinaryNode.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.binary;
+
+public class MyBinaryNode<K extends  Comparable<K>> {
+    K key;
+    MyBinaryNode<K> left;
+    MyBinaryNode<K> right;
+
+    public MyBinaryNode(K key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+
+    @Override
+    public String toString() {
+        return "MyBinaryNode{" +
+                "key=" + key +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
